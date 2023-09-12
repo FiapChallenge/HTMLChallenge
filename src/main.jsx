57d8033107382f9611efc627f5581bf2a7d24f0e
Gabriela Zanotto <126920756+GabsBecca@users.noreply.github.com>
2023-09-12 20:16:11 -0300

    Added main jsx and css

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './routes/Home/index.jsx';
+import Error from './routes/Error/index.jsx';
+import Integrantes from './routes/Integrantes/index.jsx';
+import SolicitarGuincho from './routes/SolicitarGuincho/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/contato', element: <Integrantes /> },
+      { path: '/solicitar-guincho', element: <SolicitarGuincho /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
